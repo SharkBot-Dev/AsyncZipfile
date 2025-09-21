c550b8ee55d9a957c454986d8555c2c8532efea6
sharkbot-neko <modoggu12345@outlook.com>
2025-09-21 10:31:09 +0900

    test.pyを追加

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # AsyncZipfile
 zipfileというライブラリを非同期で使えるようにしたものです。
+
+# 使い方
+```
+import async_zip
+import asyncio
+
+async def main():
+    zip = async_zip.AsyncZip()
+    await zip.create_zipfile('test.zip') # zipファイルを作成
+    await zip.write_zipfile('test.zip', 'test.txt', 'test.txt') # test.txtをtest.txtという名前でzipに保存
+
+    await zip.extract_all('test.zip', 'test') # zipをtestに解凍
+
+asyncio.run(main())
+```
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import asyncio
 
 async def main():
     zip = async_zip.AsyncZip()
-    await zip.create_zipfile('test.zip')
-    await zip.write_zipfile('test.zip', 'test.txt', 'test.txt')
+    await zip.create_zipfile('test.zip') # zipファイルを作成
+    await zip.write_zipfile('test.zip', 'test.txt', 'test.txt') # test.txtをtest.txtという名前でzipに保存
 
-    await zip.extract_all('test.zip', 'test')
+    await zip.extract_all('test.zip', 'test') # zipをtestに解凍
 
 asyncio.run(main())
